404e4dfade833478b767614b61e4d7b92cae4dab
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2022-01-20 14:23:27 -0800

    complete mvp tasks, all tests pass

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,11 +5,46 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+const testData = {
+    name: 'test name', 
+    summary: 'test summary',
+    seasons: [
+        {
+            id: 1,
+            name: 'season 1',
+            episodes: [],
+        },
+        {
+            id: 2,
+            name: 'season 2',
+            episodes: [],
+        },
+    ]
+}
 
-test('renders without errors with no props', ()=>{
+test('renders without errors with no props', () => {
+    render(<Display />);
+});
+
+test('renders Show component when the button is clicked ', async ()=> {
+    render(<Display />);
+
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const show = await screen.findByTestId(/show-container/i);
+
+    expect(show).toBeInTheDocument();
 
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders show season options matching your data when the button is clicked', async () => {
+    render(<Display show={testData} />);
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const seasons = await screen.findAllByTestId(/season-option/i);
+
+    expect(seasons).toHaveLength(4);
+});
